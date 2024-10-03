eac16963e461f326ba3573f4d69d6788ec589985
cpcsyangstu <cpcs.yang.stu@cedarpark.org>
2024-10-03 10:51:08 -0700

    prints basic board

---
+++ tic-tac-toe.py
@@ -0,0 +1,19 @@
+board = [['.', '.', '.'],
+         ['.', '.', '.'],
+         ['.', '.', '.']]
+
+def print_board():
+    print(board[0])
+    print(board[1])
+    print(board[2])
+
+def play():
+    pass
+# main program -----------------
+# print_instructions()
+
+print_board()
+# if no winner then (repeat)
+# play()
+print_board()
+# check_winner()
